fcb2c358db0746a3013e3b651089f18da9abce1a
juana <juanakoczy@gmail.com>
2023-04-01 03:41:59 +0200

    correction 1

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int nota, cont=0,suma;
-    printf("Ingrese las notas del alumno, 0:\n");
+    int nota, cont=0,suma=0;
+    float prom;
+    printf("Ingrese las notas del alumno, -1 para terminar:\n");
     scanf ("%d",&nota);
-    do {
-        if (nota!=0)
-            cont++;
-        suma=nota;
-        suma+=nota;
-    }while (nota!=0);
-    printf ("El primedio es:%d",suma/cont);
+    while (nota!=-1){
+        cont++;
+        printf("Ingrese las notas del alumno, -1 para terminar:\n");
+        scanf ("%d",&nota);
+        suma=suma+nota;
+
+    }
+    prom = suma/cont;
+
+    printf ("El promedio es:%.2f",prom);
     return 0;
 }
